5783ac3c90b74f5f1dd003356a4a809e5b54685d
alumno <alumno>
2019-02-08 12:34:49 -0300

    ejercicios

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,21 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+	var primerNumero;
+	var segundoNumero;
+
+
+	primerNumero = numeroDividendo.value;
+	segundoNumero = numeroDivisor.value;
+
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+
+
+	alert(primerNumero%segundoNumero);
+
+ 
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,23 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var resultado;
+	var cuenta;
+
+
+	sueldo = sueldo.value;
+	resultado = resultado.value;
+
+
+	sueldo=parseInt(primerNumero);
+	
+	cuenta = (sueldo * 1.10);
+
+	cuenta.value = Resultado;
+
+
+	
 
 	
 }

7e031397af37df28ec769b67751bc0aec847301a
unknown <carreracristian849@gmail.com>
2019-02-24 14:31:45 -0300

    if 8

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,5 +1,18 @@
 function mostrar()
 {
+	var suEdad;
+	var eCivil;
+
+	suEdad = document.getElementById('edad').value;
+	eCivil = document.getElementById('estadoCivil').value;
+
+
+	if (suEdad > 17 && eCivil == "Soltero")
+	{
+
+		alert("Es soltero y no es menor");
+		
+	}
 //tomo la edad  
 
 	

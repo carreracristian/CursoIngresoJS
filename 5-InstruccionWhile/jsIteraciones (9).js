9ba73cfae36713a923692fcd12bfb6aac1ebfe47
alumno <alumno>
2019-02-28 10:09:21 -0300

    ej 9 while

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,38 @@
 function mostrar()
 {
 
-	var contador=0;
+	var contador;
+	var numeroMaximo;
+	var numeroMinimo;
+	var numreroIngresado;
+	var respuesta;
 	// declarar variables
 	
-	var respuesta='si';
+	respuesta='si';
+	contador=0;
+	numeroMaximo=-9999;
+	numeroMinimo=9999;
 
 	while(respuesta!='no')
 	{
+		numreroIngresado=prompt("Ingrese numero");
+		numreroIngresado=parseInt(numreroIngresado);
 		
-	
+		if (numeroMaximo<numreroIngresado) 
+		{
+			numeroMaximo=numreroIngresado;
+		}
+		
+		if (numeroMinimo>numreroIngresado) 
+		{
+			numeroMinimo=numreroIngresado;
+		}
+		respuesta=prompt("Ingrese no para salir");
 	}
 
-
+	//console.log("el maximo es "+numeroMaximo);
+	maximo.value=numeroMaximo;
+	minimo.value=numeroMinimo;
 
 
 }//FIN DE LA FUNCIÓN

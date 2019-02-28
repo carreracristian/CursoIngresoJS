d9c05e15268f90f79a9c01ec8d8a34bc03951ded
alumno <alumno>
2019-02-28 09:36:14 -0300

    ej 8 while

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,11 +1,42 @@
 function mostrar()
 {
 
-	var contador=0;
-	var positivo=0;
-	var negativo=1;
+	var contador;
+	var positivo;
+	var negativo;
+
+	contador=0;
+	positivo=0;
+	negativo=1;
+
 	
 	var respuesta='si';
+	var contadorAlumnos=0;
+	var edad;
+	//var queresContinuar= "si";
+
+	//while(queresContinuar!= "no")
+	while(contadorAlumnos<)
+	{
+		edad=prompt("Ingrese edad");
+		edad=parseInt(edad);
+		if (edad==66) 
+		{
+
+			break;
+		}
+		if (contadorAlumnos==100) 
+		{
+			break;
+		}
+		contadorAlumnos=contadorAlumnos+1; //contadorDeVueltas=++
+		//queresContinuar=prompt("para salir ingrese no");
+	}
+
+	if (contadorAlumnos==100) 
+	{
+		
+	}
 
 
 document.getElementById('suma').value=positivo;

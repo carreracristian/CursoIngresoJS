164350274a6094e28e0bb345471b577fedfe4aaa
alumno <alumno>
2019-02-28 12:16:37 -0300

    ej 10 while

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,17 +1,61 @@
 function mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+	var contador;
+	var numeroNegativo;
+	var numeroPositivo;
+	var numreroIngresado;
+	var respuesta;
+	var conCero;
+	var contadorPares;
 
-	while(respuesta!="no")
-	{
-		
+	var contadorPositivos;
+	var contadorNegativos;
+	contador=0
+	numeroNegativo=0;
+	numeroPositivo=0;
+	numreroIngresado=0;
+	respuesta='si';
+	contadorNegativos=0;
+	contadorPositivos=0;
+	conCero=0;
+	contadorPares=0;
 	
+	while(respuesta!='no')
+	{
+		numreroIngresado=prompt("Ingrese numero");
+		numreroIngresado=parseInt(numreroIngresado);
+
+		if (numreroIngresado==0) 
+			{
+				conCero++;
+			}
+			else if (numreroIngresado<0) 
+			{
+				numeroNegativo=numeroNegativo+numreroIngresado;
+				contadorNegativos++;
+			}
+				else
+				{
+				numeroPositivo=numeroPositivo+numreroIngresado;
+				contadorPositivos++;
+				}
+				if (numreroIngresado%2==0) 
+				{
+					contadorPares++;
+				}
+			
+		
+		respuesta=prompt("Ingrese no para salir");
 	}
 
+	
+	console.log("Suma positivo "+ numeroPositivo);
+	console.log("Suma negativo "+ numeroNegativo);
+	console.log("Cantidad positivo "+contadorPositivos);
+	console.log("Cantidad negativo "+contadorNegativos);
+	console.log("cantidad de cero "+ conCero);
+	console.log("Cantidad de pares "+ contadorPares);
 
 
 
